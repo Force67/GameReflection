9ede01d594f1e3e5c7ef61f6b3d37ad6c6e5e004
Force67 <prelink835@gmail.com>
2021-08-09 19:55:41 +0200

    bugfix

--- reflection/config.cc
+++ reflection/config.cc
@@ -18,14 +18,14 @@ constexpr StringRef ArgPrefixLong = "--";
 
 std::string ConfigPath(char* arg0) {
   void* main_address = reinterpret_cast<void*>(&ConfigPath);
-  std::string path = llvm::sys::fs::getMainExecutable(arg0, main_address);
-  llvm::SmallString<256> buf{sys::path::parent_path(path)};
-  llvm::sys::path::append(buf, kConfigFileName);
+  std::string path = sys::fs::getMainExecutable(arg0, main_address);
+  SmallString<256> buf{sys::path::parent_path(path)};
+  sys::path::append(buf, kConfigFileName);
   return std::string(buf.str());
 }
 
 // positional args don't start with those two characters.
-bool IsPositionalArg(llvm::StringRef ref) {
+bool IsPositionalArg(const StringRef ref) {
   if (ref.find_first_of(ArgPrefix) == StringRef::npos &&
       ref.find_first_of(ArgPrefixLong) == StringRef::npos) {
     return true;
@@ -38,15 +38,15 @@ bool IsPositionalArg(llvm::StringRef ref) {
 void InitOpts(int argc, char** argv, const char* const desc) {
   assert(argc <= kMaxArgs);
 
-  llvm::BumpPtrAllocator alloc;
-  llvm::StringSaver storage(alloc);
+  BumpPtrAllocator alloc;
+  StringSaver storage(alloc);
   // in order to preserve the order - pun intended, we need to use
   // vector since neither set nor unordered_set can keep insertion
   // order stable
   // UniqueVector also doesn't solve the problem of comparing substrings,
   // so now we use a regular vector with our own uniqueness check
-  llvm::SmallVector<const char*, kMaxArgs> new_args;
-  auto unique_add = [&new_args](const llvm::StringRef ref) {
+  SmallVector<const char*, kMaxArgs> new_args;
+  auto unique_add = [&new_args](const StringRef ref) {
     const size_t pos = ref.find_first_of('=');
     // this is since we want to store the whole switch
     // and not a subset, so we cant ensure uniqueness.
@@ -55,7 +55,7 @@ void InitOpts(int argc, char** argv, const char* const desc) {
       // bool is_found
       if (pos != StringRef::npos) {
         const size_t p2 = arg.find_first_of('=');
-        if (p2 != std::string::npos) {
+        if (p2 != StringRef::npos) {
           // is_found = true;
           return ref.substr(0, pos).str() == arg.substr(0, p2);
         }
@@ -68,19 +68,19 @@ void InitOpts(int argc, char** argv, const char* const desc) {
     }
   };
 
-  llvm::SmallVector<const char*, 16> cfg_args;
+  SmallVector<const char*, 16> cfg_args;
   cl::readConfigFile(ConfigPath(argv[0]), storage, cfg_args);
   // first we add the real command line args
   // these will take precedence since they are added first
   for (int i = 0; i < argc; i++) {
-    const llvm::StringRef arg(argv[i]);
+    const StringRef arg(argv[i]);
     unique_add(arg);
   }
   // now we add the remaining args from config
   // note that positional arguments are explicitly forbidden and
   // are therefore ignored
   for (const char* c : cfg_args) {
-    const llvm::StringRef arg(c);
+    const StringRef arg(c);
     if (!IsPositionalArg(arg)) {
       unique_add(arg);
     }
